677aebd695c2fb52909db372610b46a8c50ca7cd
Саша <alexwebseo777@gmail.com>
2019-03-12 23:03:02 +0200

    1.0.3

--- dist/js/main.js
+++ dist/js/main.js
@@ -9,6 +9,15 @@ $(document).ready(function(){
     dots: true,
     arrows:false,
   });
+  $('.what-they-say__blog-slider-items').slick({
+    infinite: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true,
+    arrows:true,
+    prevArrow: $('.prev'),
+    nextArrow: $('.next')
+  });
 });
 
 
